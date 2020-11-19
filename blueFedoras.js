2107584324c00202e3651dafcd48de6dd644e759
rijen <rijen@redhat.com>
2020-11-19 12:39:27 -0500

    rhats-81 added default fedora color

--- blueFedoras.js
+++ blueFedoras.js
@@ -1,4 +1,10 @@
-if (blueFedoras) {
-  console.log('blue fedoras');
-} else {
-  console.log("brown fedoras");
+switch(fedoras) {
+  case "blue" :
+    console.log("blue fedoras");
+    break;
+  case "brown":
+    console.log("brown fedoras");
+    break;
+  default:
+    console.log("red fedoras");
+}
